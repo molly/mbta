e3aa03068c5d1818c7bfdfa66e63ca0db3878494
Molly White <molly.white5@gmail.com>
2015-06-21 14:54:45 -0400

    Separate tweet objects, add serialization tests

--- src/test/java/net/mollywhite/mbta/api/TweetTest.java
+++ src/test/java/net/mollywhite/mbta/api/TweetTest.java
@@ -10,19 +10,22 @@ import java.util.Collections;
 import java.util.List;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TweetTest {
   private static final ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  private final Tweet tweetWithReplyTo;
+  private final Tweet retweet;
+  private final Tweet tweetWithPlace;
 
-  @Test
-  public void tweetWithReplyToDeserializesFromJSON() throws Exception {
+  public TweetTest() {
     List<UserMention> users = new ArrayList<UserMention>();
     users.add(new UserMention("150334831", Lists.newArrayList(0, 5), "MBTA", "MBTA"));
     users.add(new UserMention("2535372901", Lists.newArrayList(6, 14), "MBTA Commuter Rail", "MBTA_CR"));
     users.add(new UserMention("95431448", Lists.newArrayList(15, 27), "The Boston Globe", "BostonGlobe"));
     users.add(new UserMention("16558796", Lists.newArrayList(28, 34), "7News Boston", "7News"));
-    Tweet expectedReplyTo = new Tweet(
+
+    tweetWithReplyTo = new Tweet(
         "Sun Jun 14 22:34:47 +0000 2015",
         "610213825237884929",
         "@MBTA @MBTA_CR @BostonGlobe @7News in this case silence is not Golden can someone share with the people on this train when they will be off?",
@@ -50,13 +53,7 @@ public class TweetTest {
         "1434321287818"
     );
 
-    Tweet actualReplyTo = mapper.readValue(fixture("fixtures/TweetWithReplyToFixture.json"), Tweet.class);
-    assertEquals(expectedReplyTo, actualReplyTo);
-  }
-
-  @Test
-  public void retweetDeserializesFromJson() throws Exception {
-    Tweet expectedRetweet = new Tweet(
+    retweet = new Tweet(
         "Sun Jun 21 17:27:47 +0000 2015",
         "612673280051388416",
         "RT @MBTA: #MBTA #OrangeLine evening Wellington - Oak Grove svc suspension starts 6/22 to make the subway more winter resilient: http://t.co\u2026",
@@ -110,13 +107,7 @@ public class TweetTest {
         "1434907667540"
     );
 
-    Tweet actualRetweet = mapper.readValue(fixture("fixtures/RetweetFixture.json"), Tweet.class);
-    assertEquals(expectedRetweet, actualRetweet);
-  }
-
-  @Test
-  public void tweetWithPlaceDeserializesFromJson() throws Exception {
-    Tweet expectedPlace = new Tweet(
+    tweetWithPlace = new Tweet(
         "Sun Jun 21 17:56:58 +0000 2015",
         "612680625368264704",
         "@MBTA, Customers can handle the bad better if you let them distract themselves easier on commuter rail and t lines. #manthisisdialup",
@@ -146,8 +137,41 @@ public class TweetTest {
         ),
         "1434909418800"
     );
+  }
 
-    Tweet actualPlace = mapper.readValue(fixture("fixtures/TweetWithPlaceFixture.json"), Tweet.class);
-    assertEquals(expectedPlace, actualPlace);
+  @Test
+  public void tweetWithReplyToSerializesToJson() throws Exception {
+    final String expected = mapper.writeValueAsString(mapper.readValue(fixture("fixtures/TweetWithReplyToFixture.json"), Tweet.class));
+    assertThat(mapper.writeValueAsString(tweetWithReplyTo)).isEqualTo(expected);
+  }
+
+  @Test
+  public void tweetWithReplyToDeserializesFromJSON() throws Exception {
+    final Tweet actualReplyTo = mapper.readValue(fixture("fixtures/TweetWithReplyToFixture.json"), Tweet.class);
+    assertThat(tweetWithReplyTo).isEqualTo(actualReplyTo);
+  }
+
+  @Test
+  public void retweetSerializesToJson() throws Exception {
+    final String expected = mapper.writeValueAsString(mapper.readValue(fixture("fixtures/RetweetFixture.json"), Tweet.class));
+    assertThat(mapper.writeValueAsString(retweet)).isEqualTo(expected);
+  }
+
+  @Test
+  public void retweetDeserializesFromJson() throws Exception {
+    final Tweet actualRetweet = mapper.readValue(fixture("fixtures/RetweetFixture.json"), Tweet.class);
+    assertThat(retweet).isEqualTo(actualRetweet);
+  }
+
+  @Test
+  public void tweetWithPlaceSerializesToJson() throws Exception {
+    final String expected = mapper.writeValueAsString(mapper.readValue(fixture("fixtures/TweetWithPlaceFixture.json"), Tweet.class));
+    assertThat(mapper.writeValueAsString(tweetWithPlace)).isEqualTo(expected);
+  }
+
+  @Test
+  public void tweetWithPlaceDeserializesFromJson() throws Exception {
+    final Tweet actualPlace = mapper.readValue(fixture("fixtures/TweetWithPlaceFixture.json"), Tweet.class);
+    assertThat(tweetWithPlace).isEqualTo(actualPlace);
   }
 }
