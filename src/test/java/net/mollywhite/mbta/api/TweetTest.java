90ed0afd8896a5290434c5498f5593f385a923ea
Molly White <molly.white5@gmail.com>
2015-06-21 13:26:04 -0400

    Fix Tweet test

--- src/test/java/net/mollywhite/mbta/api/TweetTest.java
+++ src/test/java/net/mollywhite/mbta/api/TweetTest.java
@@ -2,7 +2,12 @@ package net.mollywhite.mbta.api;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.junit.Assert.assertEquals;
@@ -12,6 +17,11 @@ public class TweetTest {
 
   @Test
   public void deserializesFromJSON() throws Exception {
+    List<UserMention> users = new ArrayList<UserMention>();
+    users.add(new UserMention("150334831", Lists.newArrayList(0, 5), "MBTA", "MBTA"));
+    users.add(new UserMention("2535372901", Lists.newArrayList(6, 14), "MBTA Commuter Rail", "MBTA_CR"));
+    users.add(new UserMention("95431448", Lists.newArrayList(15, 27), "The Boston Globe", "BostonGlobe"));
+    users.add(new UserMention("16558796", Lists.newArrayList(28, 34), "7News Boston", "7News"));
     Tweet expected = new Tweet(
         "Sun Jun 14 22:34:47 +0000 2015",
         "610213825237884929",
@@ -19,15 +29,27 @@ public class TweetTest {
         "610210856945606656",
         "150334831",
         "MBTA",
-        new User(),
+        new User(
+            "243519639",
+            "John Casey",
+            "johncaseywick",
+            "http://pbs.twimg.com/profile_images/2789497755/aee93b828a8f95c8d5b0eb14e82947ad_normal.jpeg",
+            "https://pbs.twimg.com/profile_images/2789497755/aee93b828a8f95c8d5b0eb14e82947ad_normal.jpeg"
+        ),
         null,
         null,
         null,
         0,
         0,
-        new Entities(),
+        new Entities(
+            Collections.<Hashtag> emptyList(),
+            null,
+            Collections.<URL> emptyList(),
+            users
+        ),
         "1434321287818"
     );
+
     Tweet actual = mapper.readValue(fixture("fixtures/TweetWithReplyToFixture.json"), Tweet.class);
     assertEquals(expected, actual);
   }
