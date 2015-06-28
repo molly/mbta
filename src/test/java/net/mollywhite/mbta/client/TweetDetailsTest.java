b8cdae556940c349b4578d485be7e4e86dc5eae9
Molly White <molly.white5@gmail.com>
2015-06-28 00:03:44 -0400

    Add TweetDetails test

--- src/test/java/net/mollywhite/mbta/client/TweetDetailsTest.java
+++ src/test/java/net/mollywhite/mbta/client/TweetDetailsTest.java
@@ -1,13 +1,39 @@
 package net.mollywhite.mbta.client;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.mollywhite.mbta.api.Line;
+import net.mollywhite.mbta.api.Station;
+import net.mollywhite.mbta.api.Tweet;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TweetDetailsTest {
+  private ObjectMapper mapper;
+
+  @Before
+  public void setUp() throws Exception {
+    mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
 
   @Test
-  public void testGet() throws Exception {
+  public void testGetReplyTo() throws Exception {
+    final Tweet tweet = mapper.readValue(fixture("fixtures/TweetWithReplyToFixture.json"), Tweet.class);
+    final TweetDetails tweetDetails = new TweetDetails(tweet).get();
+    assertThat(tweetDetails.getLines()).isEmpty();
+    assertThat(tweetDetails.getBranches()).isEmpty();
+    assertThat(tweetDetails.getStations()).isEmpty();
+  }
 
+  @Test
+  public void testRetweet() throws Exception {
+    final Tweet tweet = mapper.readValue(fixture("fixtures/RetweetFixture.json"), Tweet.class);
+    final TweetDetails tweetDetails = new TweetDetails(tweet).get();
+    assertThat(tweetDetails.getLines()).containsExactly(Line.ORANGE);
+    assertThat(tweetDetails.getBranches()).isEmpty();
+    assertThat(tweetDetails.getStations()).containsExactly(Station.WELLINGTON, Station.OAKGROVE);
   }
 }
