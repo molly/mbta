4bd2b064e2abe70b7029c82a4fe5b80a4ea2e5a8
Molly White <molly.white5@gmail.com>
2015-07-03 16:32:03 -0400

    Alright reverting that and trying again...

--- src/main/java/net/mollywhite/mbta/dao/TweetDAO.java
+++ src/main/java/net/mollywhite/mbta/dao/TweetDAO.java
@@ -12,11 +12,11 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public interface TweetDAO {
-  @SqlUpdate("INSERT INTO tweets (tweet, time, line, branch, station, vehicles, direction, image, retweet, official, category)" +
-      " VALUES (:tweet, :time, :line, :branch, :station, :vehicles, :direction, :image, :retweet, :official, :category)")
+  @SqlUpdate("INSERT INTO tweets (tweet, time, line, branch, station, vehicle, direction, image, retweet, official, category)" +
+      " VALUES (:tweet, :time, :line, :branch, :station, :vehicle, :direction, :image, :retweet, :official, :category)")
   void insert(@Bind("tweet") String tweet, @Bind("time") Timestamp time, @Bind("line") Array lines,
               @Bind("branch") Array branches, @Bind("station") Array stations,
-              @Bind("vehicles") Array vehicles, @Bind("direction") Direction direction, @Bind("image") Boolean hasImage,
+              @Bind("vehicle") Array vehicles, @Bind("direction") Direction direction, @Bind("image") Boolean hasImage,
               @Bind("retweet") Boolean isRetweet, @Bind("official") Boolean isOfficial,
               @Bind("category") String category);
 
