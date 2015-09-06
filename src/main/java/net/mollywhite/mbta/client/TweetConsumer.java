e544037cf2ba2821f44c20d8213ec9c3edb07cb1
Molly White <molly.white5@gmail.com>
2015-09-06 18:15:34 -0400

    Add string similarity comparison

--- src/main/java/net/mollywhite/mbta/client/TweetConsumer.java
+++ src/main/java/net/mollywhite/mbta/client/TweetConsumer.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 
@@ -41,7 +43,9 @@ public class TweetConsumer implements Runnable {
         tweetStr = messageQueue.take();
         try {
           Tweet tweet = mapper.readValue(tweetStr, Tweet.class);
-          insertTweet(tweet);
+          if (shouldInsertTweet(tweet)) {
+            insertTweet(tweet);
+          }
         } catch (IOException | SQLException e)  {
           e.printStackTrace();
           logger.error("Couldn't parse tweet: {}", tweetStr);
@@ -50,6 +54,58 @@ public class TweetConsumer implements Runnable {
         logger.info("Shutting down Tweet consumer.");
       }
     }
+  }
+
+  boolean shouldInsertTweet(Tweet tweet) {
+    String tweetText = tweet.getText().toLowerCase();
+    Timestamp offset = Timestamp.from(OffsetDateTime.now().minusHours(2).toInstant());
+    List<TweetDetails> recentStoredTweets = tweetDAO.getTweetsByHour(offset);
+    for (TweetDetails stored : recentStoredTweets) {
+      String storedText = stored.getLowercaseTweetText();
+      double longestSubstring = this.longestSubstring(tweetText, storedText);
+      double shortestStringLength = tweetText.length() < storedText.length() ? tweetText.length() : storedText.length();
+      if ((longestSubstring / shortestStringLength) > 0.5) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  int longestSubstring(String first, String second) {
+    /* Taken from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#Java, CC-BY-SA 3.0 */
+    if (first == null || second == null || first.length() == 0 || second.length() == 0) {
+      return 0;
+    }
+
+    int maxLen = 0;
+    int fl = first.length();
+    int sl = second.length();
+    int[][] table = new int[fl+1][sl+1];
+
+    for(int s=0; s <= sl; s++)
+      table[0][s] = 0;
+    for(int f=0; f <= fl; f++)
+      table[f][0] = 0;
+
+
+
+
+    for (int i = 1; i <= fl; i++) {
+      for (int j = 1; j <= sl; j++) {
+        if (first.charAt(i-1) == second.charAt(j-1)) {
+          if (i == 1 || j == 1) {
+            table[i][j] = 1;
+          }
+          else {
+            table[i][j] = table[i - 1][j - 1] + 1;
+          }
+          if (table[i][j] > maxLen) {
+            maxLen = table[i][j];
+          }
+        }
+      }
+    }
+    return maxLen;
   }
 
   @VisibleForTesting
